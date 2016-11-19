31c4ded69aebea9ff8287dfe590f2e26956af00a
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2016-11-19 17:47:20 +0100

    update lib (+formatting)

--- app/main.js
+++ app/main.js
@@ -4,34 +4,34 @@ if(require('electron-squirrel-startup')) return;
 
 // App
 
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
-let mainWindow
+let mainWindow;
 
 function createWindow () {
-  mainWindow = new BrowserWindow({width: 1024, height: 720})
+  mainWindow = new BrowserWindow({width: 1024, height: 720});
 
   mainWindow.loadURL(`file://${__dirname}/index.html`);
   mainWindow.webContents.on('will-navigate', (event) => event.preventDefault());
 
   mainWindow.on('closed', function () {
-    mainWindow = null
+    mainWindow = null;
   })
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
 })
 
 app.on('activate', function () {
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
 })
 
